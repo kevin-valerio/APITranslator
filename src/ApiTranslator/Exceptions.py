ff8109c90e84e02ab7a9cf2d9a8ec7ddc319b9d2
Soutif Thomas <th.soutif@gmail.com>
2022-10-01 13:48:55 +0200

    Removed an exception not used

--- src/ApiTranslator/Exceptions.py
+++ src/ApiTranslator/Exceptions.py
@@ -1,10 +1,3 @@
-class CheckFileXMLException(Exception):
-    def __init__(self, *args, file_path: str, manager_name: str, **kwargs):
-        self.file_path = file_path
-        self.manager_name = manager_name
-        super().__init__(*args, **kwargs)
-
-
 class DeepLAuthKeyNotSetException(Exception):
     ...
 
