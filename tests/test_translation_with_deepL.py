7b5116dfb3dfb70556883d176122f3f072d10c39
Soutif Thomas <th.soutif@gmail.com>
2022-10-01 15:41:26 +0200

    Improved the unit test for DeepL
    Add new test, use of unittest

--- tests/test_translation_with_deepL.py
+++ tests/test_translation_with_deepL.py
@@ -1,28 +1,37 @@
 import src.ApiTranslator.Exceptions
 from src.ApiTranslator._deepLTranslator import DeepLTranslator
+import unittest
+from src.ApiTranslator.Exceptions import DeepLAuthKeyNotSetException
+from src.ApiTranslator.Exceptions import DeepLAuthKeyWrongException
 
 
-class TestTranslationWithDeepL:
+class TestTranslationWithDeepL(unittest.TestCase):
     translator_module: DeepLTranslator = None
 
-
-    def __init__(self):
-        self.set_translator_module()
+    def setup_class(cls):
+        cls.set_translator_module(cls)
 
     def set_translator_module(self):
         self.translator_module = DeepLTranslator(target_language_full="ENGLISH", source_language=None)
 
     def test_translation_in_french_with_auth_env(self):
         self.translator_module.auth()
-        assert (self.translator_module.translate(
-            "Je suis une phrase en français.") == self.translator_module.translate("I am a sentence in French."))
+        self.assertEqual(self.translator_module.translate(
+            "Je suis une phrase en français."), self.translator_module.translate("I am a sentence in French."))
 
     def test_translation_in_french_with_auth_failed(self):
-        self.translator_module.auth("shouldfailed")
-        assert (self.translator_module.translate(
-            "Je suis une phrase en français.") == src.ApiTranslator.Exceptions.DeepLAuthKeyWrongException)
+        try:
+            self.translator_module.auth("shouldfailed")
+        except:
+            pass
+        try:
+            self.translator_module.translate("Je suis une phrase en français.")
+        except Exception as e:
+            self.assertEqual(e.__class__.__name__, DeepLAuthKeyWrongException().__class__.__name__)
 
-    def test_first_translation_in_french_without_auth(self):
-        translator_module_to_english = DeepLTranslator(target_language_full="ENGLISH", source_language=None)
-        assert (translator_module_to_english.translate(
-            "Je suis une phrase en français.") == src.ApiTranslator.Exceptions.DeepLAuthKeyNotSetException)
+    def test_translation_in_french_without_auth(self):
+        self.translator_module.translator_object = None
+        try:
+            self.translator_module.translate("Je suis une phrase en français.")
+        except Exception as e:
+            self.assertEqual(e.__class__.__name__, DeepLAuthKeyNotSetException().__class__.__name__)
